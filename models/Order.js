238c979f8d72bb9d08d39efa7015b09e98ddb426
Harsh Bairagi <bairagiharsh405@gmail.com>
2026-01-22 18:41:37 +0530

    Create Order.js

---
+++ models/Order.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    productName: String,
+    price: Number,
+    status: {
+        type: String,
+        default: "Pending"
+    }
+});
+
+module.exports = mongoose.model("Order", orderSchema);
