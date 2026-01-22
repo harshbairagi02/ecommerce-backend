8d0a70fbc7eecfea1a531780a37ef14e1367c6cb
Harsh Bairagi <bairagiharsh405@gmail.com>
2026-01-22 19:25:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
-
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Backend is running successfully 🚀");
+});
 
 app.use("/products", require("./routes/productRoutes"));
 app.use("/orders", require("./routes/orderRoutes"));
@@ -19,4 +23,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
-
